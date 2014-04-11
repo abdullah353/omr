cbdb62b6dafc2a50e5e228bbba8138d713577263
Abdullah <mabdullah353@gmail.com>
2014-04-11 18:09:27 +0500

    Added File Choser window

--- src/OmrController.java
+++ src/OmrController.java
@@ -1,3 +1,8 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
 
 class OmrController {
 	OmrModel sheet;
@@ -6,6 +11,28 @@ class OmrController {
 	public OmrController(OmrModel sheet,OmrView view) {
 		this.sheet=sheet;
 		this.view=view;
-		view.showExplorer();
+		this.view.showExplorer();
+		this.view.button.addActionListener(new OpenL());
+	}
+	
+	
+	/*
+	 * Listening Events on Browse Click
+	 */
+	class OpenL implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			JFileChooser c = new JFileChooser();
+			int rVal = c.showOpenDialog(new JPanel());
+			if (rVal == JFileChooser.APPROVE_OPTION) {
+				view.filename.setText(c.getSelectedFile().getName());
+				view.dir.setText(c.getCurrentDirectory().toString());
+				view.hasfile = true;
+			}
+			if (rVal == JFileChooser.CANCEL_OPTION) {
+				view.filename.setText("Operation Canceled");
+				view.dir.setText("Operation Canceled");
+				view.hasfile = false;
+			}
+		}
 	}
 }
--- src/OmrView.java
+++ src/OmrView.java
@@ -1,14 +1,49 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class OmrView {
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.View;
+
+
+public class OmrView extends JFrame{
 	OmrModel sheet;
+	protected JTextField filename = new JTextField(30),
+						 dir = new JTextField(80);
+	protected boolean hasfile = false;
+	protected JButton button = new JButton("Browse");
+
+
 	public OmrView(OmrModel sheet)
 	{
 	  this.sheet=sheet;
 	}
 	
 	public boolean showExplorer(){
-		System.out.println("Showing Explorer");
+		JPanel dc = new DrawComponent();
+	    setSize(420, 150);
+	    setContentPane(dc);
+		setVisible(true);
 		return false;
 	}
-
+	
+	/*
+	 *  Drawing Components on Frame
+	 */
+	class DrawComponent extends JPanel{
+		public DrawComponent(){
+			this.add(button);
+			this.add(filename);
+			
+			filename.setEditable(false);
+		    filename.setText("Nothing Selected! Click Browse and Choose Image");
+		    
+		    //button.addActionListener(new OpenL(this));
+		}
+	}//Draw Components
+	
+	
 }
