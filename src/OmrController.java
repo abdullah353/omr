369c3ee075e594cac5c79f8dca0b29efdd623411
Abdullah <mabdullah353@gmail.com>
2014-04-12 12:43:53 +0500

    Added Scan Button Refactored Code Logic

--- src/OmrController.java
+++ src/OmrController.java
@@ -11,8 +11,9 @@ class OmrController {
 	public OmrController(OmrModel sheet,OmrView view) {
 		this.sheet=sheet;
 		this.view=view;
-		this.view.showExplorer();
-		this.view.button.addActionListener(new OpenL());
+		this.view.subView(view.browsePanel(),true);
+		this.view.browse.addActionListener(new OpenL());
+		this.view.scan.addActionListener(new Scan());
 	}
 	
 	
@@ -27,12 +28,21 @@ class OmrController {
 				view.filename.setText(c.getSelectedFile().getName());
 				view.dir.setText(c.getCurrentDirectory().toString());
 				view.hasfile = true;
+				view.toggleSubView(view.scanPanel(), true);
 			}
 			if (rVal == JFileChooser.CANCEL_OPTION) {
 				view.filename.setText("Operation Canceled");
 				view.dir.setText("Operation Canceled");
 				view.hasfile = false;
+				view.toggleSubView(view.scanPanel(), false);
 			}
+		}
+	}
+	
+	class Scan implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			System.out.println("Start Scanning");
+			
 		}
 	}
 }
--- src/OmrView.java
+++ src/OmrView.java
@@ -1,33 +1,70 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.Window;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.text.View;
 
-
+@SuppressWarnings("serial")
 public class OmrView extends JFrame{
 	OmrModel sheet;
+	JPanel main;
 	protected JTextField filename = new JTextField(30),
 						 dir = new JTextField(80);
 	protected boolean hasfile = false;
-	protected JButton button = new JButton("Browse");
+	protected JButton browse = new JButton("Browse"),
+						scan = new JButton("Scan");
 
 
-	public OmrView(OmrModel sheet)
-	{
-	  this.sheet=sheet;
+	public OmrView(OmrModel sheet){
+		this.sheet=sheet;
+		centreWindow(this);
+		setSize(600, 150);
+		main = new DrawComponent();
+	}
+
+	/*
+	 * Center Windows Frame
+	 */
+	private static void centreWindow(Window frame) {
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int x=(int)((dimension.getWidth() - 600)/2);
+		int y=(int)((dimension.getHeight() - 150)/2);
+		frame.setLocation(x, y);
 	}
 	
-	public boolean showExplorer(){
-		JPanel dc = new DrawComponent();
-	    setSize(420, 150);
-	    setContentPane(dc);
-		setVisible(true);
-		return false;
+	
+
+
+	protected void cleanView(){
+		main.removeAll();
+	}
+	protected void subView(JPanel panel,boolean state){
+		if(state) main.add(panel);
+		else main.remove(panel);
+		setContentPane(main);
+		if(main.isVisible())setVisible(true);
+	}
+	protected JPanel browsePanel(){
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(20,0, 0, 0));
+		panel.setLayout(new FlowLayout());
+		panel.add(browse);
+		filename.setEditable(false);
+		filename.setText("Nothing Selected! Click Browse and Choose Image");
+		panel.add(filename);
+		return panel;
+	}
+	public JPanel scanPanel(){
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		panel.add(scan);
+		return panel;
 	}
 	
 	/*
@@ -35,15 +72,11 @@ public class OmrView extends JFrame{
 	 */
 	class DrawComponent extends JPanel{
 		public DrawComponent(){
-			this.add(button);
-			this.add(filename);
-			
-			filename.setEditable(false);
-		    filename.setText("Nothing Selected! Click Browse and Choose Image");
-		    
-		    //button.addActionListener(new OpenL(this));
+			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		}
-	}//Draw Components
-	
-	
+		
+	}
+	protected void toggleSubView(JPanel panel,boolean state){
+		subView(panel,state);
+	}
 }
